1e1f52317aac756f3043eb176305e3cd53c76bea
Adrien <adrien@gn2.fr>
2010-05-19 13:18:35 +0100

    Adding library

---
+++ lib/api-interface.rb
@@ -0,0 +1,58 @@
+module ApiInterface
+  
+  def register(kind, object, name)
+    return false unless name
+    
+    # Below, we include a module to add methods and classes. We are
+    # using the behaviour of Module#append_features. The content
+    # of these Module will only be added to the API if it's not already
+    # defined
+    # Read this for more info: http://ruby-doc.org/core/classes/Module.html#M001636
+
+    case kind
+    # When including methods in Api
+    # object = The method to add
+    # name = Name of the method added to API
+    when :method then
+      # object
+      include Module.new {
+        class_eval <<-RUBY
+          define_method name do
+            #{object}::#{name}
+          end
+        RUBY
+      }
+      module_function name
+      public name
+
+    # When adding a module in Api
+    # object = Module to include
+    # name = Name of the new module in API
+    when :module then
+      # The module is re-included in order to hide
+      # all the private stuff from the API
+      include Module.new {
+        class_eval <<-RUBY
+          module #{name.to_s}
+             extend #{object}
+          end.freeze
+        RUBY
+      }
+
+    # When adding a module in Api
+    # object = Module to include    
+    # name = Name of the new class in API
+    when :class then
+      # The class is inherited in order to hide
+      # all the private stuff from the API
+      include Module.new {
+        class_eval <<-RUBY
+          class #{name.to_s} < #{object}
+          end.freeze
+        RUBY
+      }
+    end # case
+  end # register method
+  protected :register
+  
+end
--- test/test_api-interface.rb
+++ test/test_api-interface.rb
@@ -1,7 +1,7 @@
-require 'helper'
+require File.dirname(__FILE__) + '/test_helper'
 
 class TestApiInterface < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "be true" do
+    assert true
   end
 end
